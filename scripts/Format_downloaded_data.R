13de46a8af487a22316cacfe5b41744c1facadc0
Nasim Sahebi <sogol.sahebi@mail.utoronto.ca>
2023-12-19 13:10:20 -0500

    cleaner the code

--- scripts/Format_downloaded_data.R
+++ scripts/Format_downloaded_data.R
@@ -1,8 +1,9 @@
 # Format_downloaded_data.R
+
 # This script formats and prepares three files:
 # - Creates "CLIN.txt"(dimension is 33-11).
-# - Creates "expr_tpm.txt.gz" based on TPM data(dimension is 60591-9).
-# - Creates "expr_fpkm.txt.gz" based on FPKM data(dimension is 60591-9).
+# - Creates "expr_tpm.txt.gz" based on TPM data(dimension is 60591-33).
+# - Creates "expr_fpkm.txt.gz" based on FPKM data(dimension is 60591-33).
 
 # Define an input directory where all files exist
 input_dir <- "~/BHK lab/kevin Project/Cholangiocarcinoma/cca_subset/"
@@ -14,45 +15,22 @@ clinical <- read.csv(paste0(input_dir, "meta_data.csv"), stringsAsFactors = FALS
 # Save clinical data as CLIN.txt
 write.table(clinical, file = file.path(output_dir, 'CLIN.txt'), quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
 
-# List of all sample file names
+# Define the unique part of the sample file names.
 sample_files <- c(
-  "BTC_0004_Lv_M_526_stringtie_abundance.txt", 
-  "BTC_0009_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0010_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0011_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0012_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0013_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0014_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0015_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0016_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0017_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0018_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0019_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0020_Ab_M_526_stringtie_abundance.txt",
-  "BTC_0022_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0023_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0024_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0025_Sn_M_526_stringtie_abundance.txt",
-  "BTC_0026_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0028_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0029_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0030_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0032_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0033_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0034_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0035_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0036_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0037_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0038_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0039_Lv_M_526_stringtie_abundance.txt",
-  "BTC_0040_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0042_Lv_P_526_stringtie_abundance.txt",
-  "BTC_0045_Lv_P_526_stringtie_abundance.txt",
-  "BTC_8002_Lv_M_526_stringtie_abundance.txt"
+  "BTC_0004_Lv_M_", "BTC_0009_Lv_P_", "BTC_0010_Lv_P_", "BTC_0011_Lv_P_", 
+  "BTC_0012_Lv_P_", "BTC_0013_Lv_P_", "BTC_0014_Lv_M_", "BTC_0015_Lv_P_", 
+  "BTC_0016_Lv_M_", "BTC_0017_Lv_P_", "BTC_0018_Lv_P_", "BTC_0019_Lv_P_", 
+  "BTC_0020_Ab_M_", "BTC_0022_Lv_M_", "BTC_0023_Lv_M_", "BTC_0024_Lv_M_", 
+  "BTC_0025_Sn_M_", "BTC_0026_Lv_M_", "BTC_0028_Lv_M_", "BTC_0029_Lv_M_", 
+  "BTC_0030_Lv_M_", "BTC_0032_Lv_M_", "BTC_0033_Lv_P_", "BTC_0034_Lv_P_", 
+  "BTC_0035_Lv_M_", "BTC_0036_Lv_P_", "BTC_0037_Lv_P_", "BTC_0038_Lv_P_", 
+  "BTC_0039_Lv_M_", "BTC_0040_Lv_P_", "BTC_0042_Lv_P_", "BTC_0045_Lv_P_", 
+  "BTC_8002_Lv_M_"
 )
 
 # Function to process each file
-process_file <- function(file_name, input_dir, unique_Genename) {
+process_file <- function(unique_file_name, input_dir, unique_Genename) {
+  file_name <- paste0(unique_file_name, "526_stringtie_abundance.txt")
   file_path <- paste0(input_dir, file_name)
   data <- read.csv(file_path, stringsAsFactors = FALSE, sep = "\t")
   data <- data[order(data$Gene.Name), ]
@@ -67,7 +45,9 @@ process_file <- function(file_name, input_dir, unique_Genename) {
 }
 
 # Read the first file to get the unique gene names
-first_file_path <- paste0(input_dir, sample_files[1])
+#first_file_path <- paste0(input_dir, sample_files[1]) #path to BTC_0004..txt file.
+
+first_file_path <- paste0(input_dir, sample_files[1], "526_stringtie_abundance.txt")
 first_file_data <- read.csv(first_file_path, stringsAsFactors = FALSE, sep = "\t")
 unique_Genename <- sort(unique(first_file_data$Gene.Name))
 
