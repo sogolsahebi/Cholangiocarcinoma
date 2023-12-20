fb00057bd231f5438ac90f2c58d77c9d66dae478
Nasim Sahebi <sogol.sahebi@mail.utoronto.ca>
2023-12-20 10:39:01 -0500

    conver rownames of expr to gene_names.

--- scripts/Format_EXPR.R
+++ scripts/Format_EXPR.R
@@ -1,70 +1,70 @@
-# Format_downloaded_data.R
-
-# Script processes expr data:
+# Script to Process Expression Data
 # - Generates EXPR_TPM.csv with Log2(TPM+1)
-# - Creates SummarizedExperiment (SE) object
+# - Creates SummarizedExperiment (SE) Object - annottation : used GENCODE 40 and 38040 genes was found.
 
-# Load libraries
-library(data.table)
+
+# Load Required Libraries
+library(data.table) 
 library(SummarizedExperiment)
 library(GenomicRanges)
 
-# Directory for input/output
+# Set Directory for Input/Output
 directory <- "~/BHK lab/kevin Project/Cholangiocarcinoma/"
 
-# Reading and initial processing of expr_tpm data
-expr_tpm <- fread(paste0(directory, "files/EXPR_TPM.txt.gz"), sep = "\t", dec = ",", stringsAsFactors = FALSE)
+# Read and Process Expression TPM Data
+expr_tpm_file <- paste0(directory, "files/EXPR_TPM.txt.gz")
+expr_tpm <- fread(expr_tpm_file, sep = "\t", dec = ",", stringsAsFactors = FALSE)
 rownames_expr_tpm <- expr_tpm$V1
 expr_tpm <- expr_tpm[,-1]
 
-# Converting values to numeric
+# Convert Values to Numeric and Assign Row Names
 expr_tpm <- apply(expr_tpm, 2, as.numeric)
 rownames(expr_tpm) <- rownames_expr_tpm
 
-range(expr_tpm) # Range is 0.0 to 196317.7
+# Log2 Transformation
+expr_tpm <- log2(expr_tpm + 1)
 
-# Applying Log2 transformation
-expr_tpm <- log2(expr_tpm + 1)  #Range is 0.00000  to 17.58284 
+# Save Processed Data as CSV
+expr_tpm_csv_file <- paste0(directory, "files/EXPR_TPM.csv")
+write.csv(expr_tpm, expr_tpm_csv_file, row.names = TRUE)
 
-# Save processed data as CSV
-write.csv(expr_tpm, paste0(directory, "files/EXPR_TPM.csv"), row.names = TRUE)
+# Read Clinical Data and Ensure Column Name Match
+clin_file <- paste0(directory, "files/CLIN.txt")
+clin <- fread(clin_file, sep = "\t", stringsAsFactors = FALSE)
+stopifnot(all(colnames(expr_tpm) == clin$id))
 
-# Read clinical data
-clin <- fread(paste0(directory, "files/CLIN.txt"), sep = "\t", stringsAsFactors = FALSE)
-stopifnot(all(colnames(expr_tpm) == clin$id))  # Ensure column name match
-
-# Load and process GENCODE annotation
-load("~/BHK lab/Annotation/Gencode.v40.annotation (8).RData")
+# Load and Process GENCODE Annotation
+gencode_file <- "~/BHK lab/Annotation/Gencode.v40.annotation.RData"
+load(gencode_file)
 features_df <- features_gene
+
+# Remove Duplicated Gene Names
+features_df <- features_df[!duplicated(features_df$gene_name), ]
+
+# Filter and Order Assay Data
 assay <- expr_tpm[rownames(expr_tpm) %in% features_df$gene_name, ]
-
-# Replace assay gene names with gene ids
-gene_ids <- unlist(lapply(rownames(assay), function(assay_row){
-  vals <- rownames(features_df[features_df$gene_name == assay_row, ])
-  if(length(vals) > 1){
-    return(vals[1])
-  } else {
-    return(vals)
-  }
-}))
-
-rownames(assay) <- gene_ids
 assay <- assay[order(rownames(assay)), ]
 
-# Prepare rowData for SummarizedExperiment
-assay_genes <- features_df[rownames(features_df) %in% gene_ids, ]
+# Prepare Row Data for SummarizedExperiment
+assay_genes <- features_df[features_df$gene_name %in% rownames(assay), ]
 assay_genes$gene_id_no_ver <- gsub("\\..*$", "", assay_genes$gene_id)
 assay_genes <- assay_genes[!is.na(assay_genes$start), ]
+rownames(assay_genes) <- assay_genes$gene_name
+assay_genes <- assay_genes[order(rownames(assay_genes)), ]
+
+# Create GRanges Object for Row Ranges
 row_ranges <- makeGRangesFromDataFrame(assay_genes, keep.extra.columns = TRUE)
+names(row_ranges) <- assay_genes$gene_name
 
-names(row_ranges) <- row_ranges$rownames
+# Create Assay List
+assay_list <- list(expr_tpm = assay)
 
-assay_list <- list()
-assay_list[["expr_tpm"]] <- assay
-
-# Create SE object
+# Create SummarizedExperiment Object
 SE_CCA <- SummarizedExperiment(assays = assay_list, colData = clin, rowRanges = row_ranges)
 
-#Save the SE experiment ac rds file..
-saveRDS(SE_expr_tpm, paste0(directory, "data/SE_CCA.rds"))
+# Save the SummarizedExperiment Object
+se_cca_file <- paste0(directory, "data/SE_CCA.rds")
+saveRDS(SE_CCA, se_cca_file)
+
+
 
